03401afb2ddca8bee79099de75919b17a7e2f06c
Tobias Schuhmacher <i23036@hb.dhbw-stuttgart.de>
2025-06-14 16:58:47 +0200

    minifix

--- EventTool/ET-Backend/Services/Helper/Authentication/JwtBaererOptionsSetup.cs
+++ EventTool/ET-Backend/Services/Helper/Authentication/JwtBaererOptionsSetup.cs
@@ -5,13 +5,6 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.Extensions.Logging;
 
 namespace ET_Backend.Services.Helper.Authentication;
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.Extensions.Logging;
-
-namespace ET_Backend.Services.Helper.Authentication;
 
 public class JwtBaererOptionsSetup : IConfigureNamedOptions<JwtBearerOptions>
 {
@@ -44,39 +37,6 @@ public class JwtBaererOptionsSetup : IConfigureNamedOptions<JwtBearerOptions>
     /// Konfiguriert die <see cref="JwtBearerOptions"/> mit validierten Parametern.
     /// </summary>
     /// <param name="options">Die zu konfigurierenden Optionen.</param>
-    public void Configure(JwtBearerOptions options)
-    {
-        _logger.LogInformation("JWT Validation mit SecretKey: {Key}", _jwtOptions.SecretKey);
-
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateAudience = true,
-            ValidateIssuer = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-
-            ValidIssuer = _jwtOptions.Issuer,
-            ValidAudience = _jwtOptions.Audience,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.SecretKey))
-        };
-    }
-}
-public class JwtBaererOptionsSetup : IConfigureNamedOptions<JwtBearerOptions>
-{
-    private readonly JwtOptions _jwtOptions;
-    private readonly ILogger<JwtBaererOptionsSetup> _logger;
-
-    public JwtBaererOptionsSetup(IOptions<JwtOptions> options, ILogger<JwtBaererOptionsSetup> logger)
-    {
-        _jwtOptions = options.Value;
-        _logger = logger;
-    }
-
-    public void Configure(string name, JwtBearerOptions options)
-    {
-        Configure(options);
-    }
-
     public void Configure(JwtBearerOptions options)
     {
         _logger.LogInformation("JWT Validation mit SecretKey: {Key}", _jwtOptions.SecretKey);
